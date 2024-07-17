22460bde39fb827f69fdc0149ea4d5cb15e483b1
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-07-17 16:20:49 -0700

    Update profile.jsx

--- app/routes/home/profile.jsx
+++ app/routes/home/profile.jsx
@@ -67,7 +67,7 @@ export const Profile = ({ id, visible, sectionRef }) => {
                 secondary
                 className={styles.button}
                 data-visible={visible}
-                href="/contact"
+                href="mailto:ashishtiwari.b@gmail.com"
                 icon="send"
               >
                 Send me a message
