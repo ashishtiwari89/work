54064d3911da072c0d286ed6da0641204cf2a278
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-07-17 00:20:17 -0700

    Update navbar.jsx

--- app/layouts/navbar/navbar.jsx
+++ app/layouts/navbar/navbar.jsx
@@ -185,6 +185,7 @@ export const Navbar = () => {
                 data-visible={visible}
                 aria-current={getCurrent(pathname)}
                 onClick={handleMobileNavClick}
+                target="_self"
                 style={cssProps({
                   transitionDelay: numToMs(
                     Number(msToNum(tokens.base.durationS)) + index * 50
