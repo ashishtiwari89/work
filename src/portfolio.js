a6fa12045af9263e2cfb84a85abb7a4b8fc9a4ec
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 13:06:08 -0800

    Initial

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,25 +21,25 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1 Renamed',
+    name: 'E-Commerce Website',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'E-commerce platform using React, Node.js, MongoDB for flexible data handling, WebSockets for real-time order tracking, and Apache Kafka for event-driven messaging.',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'E-Learning Website',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'Java, SpringBoot and React-based e-learning platform with course management, quizzes, and progress tracking, leveraging MySQL for database and RabbitMQ for notifications.',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 3',
+    name: 'Collaborative Workspace',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'Collaborative workspace with task management, real-time chat, and scheduling, using Spring Boot, React, PostgreSQL for data storage, and Apache Kafka for event streaming.',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
