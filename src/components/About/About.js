9dbb77275c405d874472bfd0cd479fe98a89eaa6
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 14:17:11 -0800

    Initial

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
   const { name, role, description, resume, social } = about
 
   return (
-    <div className='about center'>
+    <div className='about center' id='about'>
       {name && (
         <h1>
           Hi, I am <span className='about__name'>{name}.</span>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
 import { ThemeContext } from '../../contexts/theme'
-import { projects, skills, contact } from '../../portfolio'
+import { projects, skills, contact, about } from '../../portfolio'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -19,6 +19,18 @@ const Navbar = () => {
         style={{ display: showNavList ? 'flex' : null }}
         className='nav__list'
       >
+        {about.length ? (
+          <li className='nav__list-item'>
+            <a
+              href='#about'
+              onClick={toggleNavList}
+              className='link link--nav'
+            >
+              About
+            </a>
+          </li>
+        ) : null}
+
         {projects.length ? (
           <li className='nav__list-item'>
             <a
