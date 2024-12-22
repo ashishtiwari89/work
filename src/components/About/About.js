634facad4ed15df357093d2a42b588be7f49e07d
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 15:22:40 -0800

    Initial

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,40 +16,6 @@ const About = () => {
 
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
-
-      <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
-      </div>
   </section>
   )
 }
