01842ce303b06584d307ae63690c0b7d03b65319
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 06:14:37 -0800

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://ashishtiwari.work',
+  homepage: 'https://ashishtiwari89.github.io/work',
   title: 'AT.',
 }
 
