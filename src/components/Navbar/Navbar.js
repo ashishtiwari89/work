2dd7d193a925364e76f2d0da56d13e32f7435c73
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 15:07:23 -0800

    Initial

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
         style={{ display: showNavList ? 'flex' : null }}
         className='nav__list'
       >
-         {about.length ? (
+         
           <li className='nav__list-item'>
             <a
               href='#about'
@@ -29,7 +29,7 @@ const Navbar = () => {
               About
             </a>
           </li>
-          ) : null}
+          
 
         {projects.length ? (
           <li className='nav__list-item'>
