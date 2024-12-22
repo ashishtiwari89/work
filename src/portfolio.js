e9e2dd621de4a45a83cd3ef7a27319d783eef550
Ashish Tiwari <56654663+ashishtiwari89@users.noreply.github.com>
2024-12-22 05:29:44 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,13 +1,13 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://ashishtiwari89.github.io/work',
-  title: 'AT.',
+  title: 'Ashish Tiwari',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Ashish Tiwari',
-  role: 'Full Stack Software Engineer',
+  role: 'Full Stack Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
